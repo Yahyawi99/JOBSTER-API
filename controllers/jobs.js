51ccd3df31cd2805e10e1c0caa532fa9be729b37
Yahyawi99 <yassinyahyawi26@gmail.com>
2023-09-17 14:35:28 +0100

    implement pagination

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -40,9 +40,18 @@ const getAllJobs = async (req, res) => {
     }
   }
 
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
   const jobs = await result;
 
-  res.status(StatusCodes.OK).json({ jobs, count: jobs.length });
+  const totalJobs = await Job.countDocuments(queryObject);
+
+  const numOfPages = Math.ceil(totalJobs / 10);
+
+  res.status(StatusCodes.OK).json({ jobs, totalJobs, numOfPages });
 };
 
 // get single job
